80e48914f6782d42c5deac61bfd1a33b6390a4c5
Alex Omarov <33569929+AlexOmarov@users.noreply.github.com>
2022-03-22 16:07:47 +0700

    BERTE-3 Adding first kotlin idiomatic test
    First tests

---
+++ src/test/kotlin/ru/somarov/berte/hessian/HessianEncoderTests.kt
@@ -0,0 +1,39 @@
+package ru.somarov.berte.hessian
+
+import com.caucho.hessian.io.HessianSerializerInput
+import com.caucho.hessian.io.HessianSerializerOutput
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.core.io.buffer.DataBufferUtils
+import org.springframework.core.io.buffer.DefaultDataBufferFactory
+import ru.somarov.berte.hessian.impl.HessianDecoder
+import ru.somarov.berte.hessian.impl.HessianEncoder
+import ru.somarov.dto.SimpleMessage
+import java.util.*
+
+class HessianEncoderTests {
+
+    @Test
+    fun `Encoder encodes incoming data into data buffer`() {
+        val encoder = HessianEncoder()
+        val uuid = UUID.randomUUID()
+        val value = "TEST"
+        val message = SimpleMessage(value, uuid)
+        val buffer = encoder.encode(message,DefaultDataBufferFactory())
+
+
+        val inpStr = buffer.asInputStream()
+        val hessianSerializerInput = HessianSerializerInput(inpStr)
+
+        hessianSerializerInput.startMessage()
+        val result: SimpleMessage = hessianSerializerInput.readObject() as SimpleMessage
+        hessianSerializerInput.completeMessage()
+
+        hessianSerializerInput.close()
+        inpStr.close()
+
+        DataBufferUtils.release(buffer)
+
+        Assertions.assertTrue(result.value == value && result.id == uuid)
+    }
+}
