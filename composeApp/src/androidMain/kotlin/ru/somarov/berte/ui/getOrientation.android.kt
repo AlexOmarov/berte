680dcdec84401f4bff77bd6e19861b36bac7934b
serg <somarov@gmail.com>
2024-07-09 14:48:30 +0700

    LoginScreen refactoring

---
+++ composeApp/src/androidMain/kotlin/ru/somarov/berte/ui/getOrientation.android.kt
@@ -0,0 +1,19 @@
+package ru.somarov.berte.ui
+
+import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalWindowInfo
+
+@Composable
+actual fun rememberOrientation(): Orientation {
+    val configuration = LocalConfiguration.current
+    return remember(configuration.orientation) {
+        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Orientation.LANDSCAPE
+        } else {
+            Orientation.PORTRAIT
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/ru/somarov/berte/ui/screen/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/somarov/berte/ui/screen/LoginScreen.kt
@@ -1,27 +1,43 @@
 package ru.somarov.berte.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.outlined.Apps
+import androidx.compose.material.icons.outlined.Facebook
+import androidx.compose.material.icons.outlined.GMobiledata
+import androidx.compose.material.icons.outlined.GetApp
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.ViewKanban
 import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,6 +46,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.launch
@@ -38,7 +55,9 @@ import ru.somarov.berte.application.viewmodel.AppViewModel
 import ru.somarov.berte.application.viewmodel.LoginScreenViewModel
 import ru.somarov.berte.infrastructure.network.CommonResult
 import ru.somarov.berte.ui.ErrorBox
+import ru.somarov.berte.ui.Orientation
 import ru.somarov.berte.ui.WaitBox
+import ru.somarov.berte.ui.rememberOrientation
 
 @Composable
 @Suppress("CyclomaticComplexMethod") // refactor
@@ -48,75 +67,187 @@ fun LoginScreen(
     screenViewModel: LoginScreenViewModel = viewModel { LoginScreenViewModel() }
 ) {
     val loading by screenViewModel.loginProgress.collectAsState()
+    val orientation = rememberOrientation()
+    when (val ld = loading) {
+        is CommonResult.Empty -> {
+            when(orientation) {
+                Orientation.PORTRAIT ->  LoginScreenContentPortrait(
+                    viewModel = viewModel,
+                    screenViewModel = screenViewModel,
+                    modifier = Modifier.verticalScroll(
+                        rememberScrollState()
+                    )
+                )
+                Orientation.LANDSCAPE -> LoginScreenContentLandscape(viewModel, screenViewModel)
+            }
+        }
+        is CommonResult.Error -> ErrorBox(ld.error.message ?: "error")
+        is CommonResult.Loading -> WaitBox()
+        is CommonResult.Success -> viewModel.navigateTo(UIScreen.Home)
+    }
+}
 
-    val scope = rememberCoroutineScope()
 
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+@Composable
+private fun UsernameOutlinedText(
+    username: String,
+    modifier: Modifier = Modifier,
+    setUsername: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = username,
+        onValueChange = setUsername,
+
+        label = {
+            Text("Username")
+        },
+        singleLine = true,
+        leadingIcon = {
+            IconButton(
+                onClick = {
+                    setUsername("")
+                }, enabled = username.isNotEmpty()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "очистить поле"
+                )
+            }
+        },
+        shape = MaterialTheme.shapes.extraLarge,
+        modifier = modifier
+    )
+}
+@Composable
+private fun PasswordOutlinedText(
+    password: String,
+    modifier: Modifier = Modifier,
+    setPassword: (String) -> Unit
+) {
     var showPassword by remember { mutableStateOf(false) }
     val transformation = remember(showPassword) {
         if (showPassword) VisualTransformation.None else PasswordVisualTransformation()
     }
 
-    @Suppress("LongMethod", "CyclomaticComplexMethod") // refactor
-    @Composable
-    fun LoginScreenContent(modifier: Modifier = Modifier) {
-        Column(modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+    OutlinedTextField(
+        value = password,
+        onValueChange = setPassword,
 
-            Spacer(Modifier.height(90.dp))
+        label = {
+            Text("Password")
+        },
+        singleLine = true,
+        modifier = modifier,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
+        ),
+        visualTransformation = transformation,
+        leadingIcon = {
+            IconButton(
+                onClick = {
+                    setPassword("")
+                }, enabled = password.isNotEmpty()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "очистить поле"
+                )
+            }
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                showPassword = showPassword.not()
+            }) {
+                Icon(
+                    imageVector = if (showPassword)
+                        Icons.Default.Visibility
+                    else
+                        Icons.Default.VisibilityOff,
+                    contentDescription = "видимость пароля"
+                )
+            }
+        },
+        shape = MaterialTheme.shapes.extraLarge,
+    )
+}
 
-            OutlinedTextField(
-                value = username,
-                onValueChange = { username = it },
-                label = {
-                    Text("Username")
-                },
-                modifier = Modifier.padding(16.dp).fillMaxWidth()
-            )
-
-            Spacer(Modifier.height(20.dp))
-
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = {
-                    Text("Password")
-                },
-                modifier = Modifier.padding(16.dp).fillMaxWidth(),
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                ),
-                visualTransformation = transformation,
-                leadingIcon = (@Composable {
-                    IconButton(
-                        onClick = {
-                            password = ""
-                        }) {
-                        Icon(
-                            imageVector = Icons.Default.Clear,
-                            contentDescription = "очистить поле"
-                        )
-                    }
-                }).takeIf { password.isNotEmpty() },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        showPassword = showPassword.not()
-                    }) {
-                        Icon(
-                            imageVector = if (showPassword)
-                                Icons.Default.Visibility
-                            else
-                                Icons.Default.VisibilityOff,
-                            contentDescription = "видимость пароля"
-                        )
-                    }
+@Suppress("LongMethod", "CyclomaticComplexMethod") // refactor
+@Composable
+private fun LoginScreenContentPortrait(
+    viewModel: AppViewModel,
+    screenViewModel: LoginScreenViewModel,
+    modifier: Modifier = Modifier
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column(modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(Modifier.height(20.dp))
+        Text("Login in Berte", style = MaterialTheme.typography.displayMedium)
+        Spacer(Modifier.height(20.dp))
+        UsernameOutlinedText(username,Modifier.padding(16.dp).fillMaxWidth()) {
+            username = it
+        }
+        Spacer(Modifier.height(20.dp))
+        PasswordOutlinedText(password, Modifier.padding(16.dp).fillMaxWidth()) {
+            password = it
+        }
+        Button(
+            onClick = {
+                screenViewModel.viewModelScope.launch {
+                    screenViewModel.loginMock(
+                        username = username, password = password
+                    )
                 }
-            )
+            }, enabled = username.isNotEmpty() && password.isNotEmpty()
+        ) {
+            Text("Login with password")
+        }
 
+        Spacer(Modifier.height(20.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            FilledTonalIconButton(onClick = {}) {
+                Icon(Icons.Outlined.GMobiledata, contentDescription = "Google")
+            }
+            FilledTonalIconButton(onClick = {}) {
+                Icon(Icons.Outlined.Facebook, contentDescription = "Facebook")
+            }
+            FilledTonalIconButton(onClick = {}) {
+                Icon(Icons.Outlined.Apps, contentDescription = "Apple")
+            }
+            FilledTonalIconButton(onClick = {}) {
+                Icon(Icons.Outlined.Mail, contentDescription = "VK")
+            }
+        }
+        Spacer(Modifier.height(20.dp))
+        TextButton(onClick = { viewModel.navigateTo(UIScreen.Register) }) {
+            Text("Create account")
+        }
+    }
+}
+
+//@Suppress("LongMethod", "CyclomaticComplexMethod") // refactor
+@Composable
+private fun LoginScreenContentLandscape(
+    viewModel: AppViewModel,
+    screenViewModel: LoginScreenViewModel,
+    modifier: Modifier = Modifier
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(Modifier.width(350.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Login in Berte", style = MaterialTheme.typography.displayMedium)
+            Spacer(Modifier.height(20.dp))
+            UsernameOutlinedText(username,Modifier.padding(16.dp).fillMaxWidth()) {
+                username = it
+            }
+            PasswordOutlinedText(password, Modifier.padding(16.dp).fillMaxWidth()) {
+                password = it
+            }
             Button(
                 onClick = {
-                    scope.launch {
+                    screenViewModel.viewModelScope.launch {
                         screenViewModel.loginMock(
                             username = username, password = password
                         )
@@ -125,19 +256,27 @@ fun LoginScreen(
             ) {
                 Text("Login with password")
             }
-
+            Spacer(Modifier.height(20.dp))
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                FilledTonalIconButton(onClick = {}) {
+                    Icon(Icons.Outlined.GMobiledata, contentDescription = "Google")
+                }
+                FilledTonalIconButton(onClick = {}) {
+                    Icon(Icons.Outlined.Facebook, contentDescription = "Facebook")
+                }
+                FilledTonalIconButton(onClick = {}) {
+                    Icon(Icons.Outlined.Apps, contentDescription = "Apple")
+                }
+                FilledTonalIconButton(onClick = {}) {
+                    Icon(Icons.Outlined.Mail, contentDescription = "VK")
+                }
+            }
             Spacer(Modifier.height(20.dp))
 
-            Button(onClick = { viewModel.navigateTo(UIScreen.Register) }) {
-                Text("Register")
+            TextButton(onClick = { viewModel.navigateTo(UIScreen.Register) }) {
+                Text("Create account")
             }
+            Spacer(Modifier.height(40.dp))
         }
-    }
-
-    when (val ld = loading) {
-        is CommonResult.Empty -> LoginScreenContent()
-        is CommonResult.Error -> ErrorBox(ld.error.message ?: "error")
-        is CommonResult.Loading -> WaitBox()
-        is CommonResult.Success -> viewModel.navigateTo(UIScreen.Home)
     }
 }
