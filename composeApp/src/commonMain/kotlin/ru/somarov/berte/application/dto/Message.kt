f715df7bdff785e63fb120433a06a8aac0cbe78e
Alex Omarov <shtil.a@yandex.ru>
2024-07-09 22:56:14 +0700

    Added serializable
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- composeApp/src/commonMain/kotlin/ru/somarov/berte/application/dto/Message.kt
+++ composeApp/src/commonMain/kotlin/ru/somarov/berte/application/dto/Message.kt
@@ -2,9 +2,11 @@ package ru.somarov.berte.application.dto
 
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
 
 enum class MessageType { InMessage, OutMessage, Warning }
 
+@Serializable
 data class Message(
     val text: String,
     val messageType: MessageType,
