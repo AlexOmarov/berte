9a6feaf414088da0cf48e50150e0d2d770250b87
serg <somarov@gmail.com>
2024-07-11 15:19:03 +0700

    change dto, minor fixes ui elements

--- composeApp/src/commonMain/kotlin/ru/somarov/berte/application/dto/Message.kt
+++ composeApp/src/commonMain/kotlin/ru/somarov/berte/application/dto/Message.kt
@@ -4,11 +4,108 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 
+@Serializable
 enum class MessageType { InMessage, OutMessage, Warning }
 
 @Serializable
 data class Message(
     val text: String,
     val messageType: MessageType,
-    val date: Instant = Clock.System.now()
+    val date: Instant = Clock.System.now(),
+
+    // позиция в игровом мире
+    val position: MapPosition?,
+
+    // в ui элементе сообщения показывается загруженная картинка
+    val iconUrl: String? = null,
+
+    // позволяет перейти из сообщения на отдельный экран
+    val route: String? = null,
+
+    // сразу после получения проигрывает звук или проигрывает анимацию
+    // или производит какое либо иное действие с текущим UI, но не меняет
+    // дух повествования
+    val uiEvent: MessageUiEvent? = null,
+
+    // определяет настрой истории в текущий момент меняет
+    // визуальную составляющую всего экрана
+    // backgorund image, tint цвета, меняет фоновую музыку
+    val storeSpirit: MessageSpirit = MessageSpiritCase.Neutral.spirit,
+
+    // определяет настрой сообщения
+    // меняет визуальную составляющую сообщения
+    // backgorund image, tint цвета, при действии с сообщением
+    // меняет фоновую музыку
+    val messageSpirit: MessageSpirit = MessageSpiritCase.Neutral.spirit
 )
+
+@Serializable
+data class MessageUiEvent(
+    val animation: MessageAnimation? = null,
+    val sound: String?
+)
+
+// какие то случайные названия
+// в последствии на фронте будут реализованы осмысленные
+// названия которыми можно будет управлять с сервера
+@Serializable
+enum class MessageAnimation {
+    Bam,
+    Buh,
+    Trah,
+    Ura
+}
+
+@Serializable
+data class MessageSpirit(
+    val background: String,
+    val tint: String,
+    val music: String? = null,
+)
+
+// предустановленные состояния
+@Serializable
+enum class MessageSpiritCase(val spirit: MessageSpirit) {
+    Catastrophic(
+        MessageSpirit(
+            background = "http://berte.ru/store/catastrophic.png",
+            tint = "D0D0D0"
+        )
+    ),
+    Critical(
+        MessageSpirit(
+            background = "http://berte.ru/store/critical.png",
+            tint = "D0A0A0"
+        )
+    ),
+    Dangerous(
+        MessageSpirit(
+            background = "http://berte.ru/store/dangerous.png",
+            tint = "A0A0A0"
+        )
+    ),
+    Neutral(
+        MessageSpirit(
+            background = "http://berte.ru/store/neutral.png",
+            tint = "505050"
+        )
+    ),
+    Favorable(
+        MessageSpirit(
+            background = "http://berte.ru/store/favorable.png",
+            tint = "50A070"
+        )
+    ),
+    Good(
+        MessageSpirit(
+            background = "http://berte.ru/store/good.png",
+            tint = "50D070"
+        )
+    ),
+    Excellent(
+        MessageSpirit(
+            background = "http://berte.ru/store/excellent.png",
+            tint = "50D0D0"
+        )
+    ),
+}
--- composeApp/src/commonMain/kotlin/ru/somarov/berte/ui/CommonElements.kt
+++ composeApp/src/commonMain/kotlin/ru/somarov/berte/ui/CommonElements.kt
@@ -1,10 +1,12 @@
 package ru.somarov.berte.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -12,10 +14,17 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format
+import kotlinx.datetime.format.Padding
+import kotlinx.datetime.format.char
+import kotlinx.datetime.toLocalDateTime
 import ru.somarov.berte.application.dto.Message
 import ru.somarov.berte.application.dto.MessageType
 
@@ -60,9 +69,15 @@ fun ColumnScope.Messages(messages: List<Message>, modifier: Modifier = Modifier)
 
 @Composable
 fun Message(message: Message, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    val date = remember(message.date) {
+        message.date.toLocalDateTime(TimeZone.currentSystemDefault())
+            .format(SimpleDateTimeFormat)
+    }
     Card(
         onClick = onClick,
-        modifier = modifier.paddingMessage(message).fillMaxWidth(),
+        modifier = modifier.paddingMessage(message)
+            .widthIn(min = 200.dp, max = 400.dp)
+            .padding(8.dp),
         colors = if (message.messageType == MessageType.Warning)
             CardDefaults.cardColors(
                 containerColor = MaterialTheme.colorScheme.errorContainer,
@@ -74,16 +89,44 @@ fun Message(message: Message, modifier: Modifier = Modifier, onClick: () -> Unit
             defaultElevation = 4.dp,
         ),
     ) {
-        Text(
-            text = message.text,
-            modifier = Modifier.padding(all = 8.dp).fillMaxWidth(),
-            textAlign = when (message.messageType) {
-                MessageType.InMessage -> TextAlign.Left
-                MessageType.OutMessage -> TextAlign.Right
-                MessageType.Warning -> TextAlign.Center
-            }
-        )
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = date,
+                modifier = Modifier.padding(all = 8.dp).fillMaxWidth(),
+                style = MaterialTheme.typography.labelSmall,
+                textAlign = when (message.messageType) {
+                    MessageType.InMessage -> TextAlign.Left
+                    MessageType.OutMessage -> TextAlign.Right
+                    MessageType.Warning -> TextAlign.Center
+                }
+            )
+
+            Text(
+                text = message.text,
+                modifier = Modifier.padding(all = 8.dp).fillMaxWidth(),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = when (message.messageType) {
+                    MessageType.InMessage -> TextAlign.Left
+                    MessageType.OutMessage -> TextAlign.Right
+                    MessageType.Warning -> TextAlign.Center
+                }
+            )
+        }
     }
+}
+
+private val SimpleDateTimeFormat = LocalDateTime.Format {
+    dayOfMonth(padding = Padding.ZERO)
+    char('.')
+    monthNumber(padding = Padding.ZERO)
+    char('.')
+    year(padding = Padding.ZERO)
+    char(' ')
+    hour(padding = Padding.ZERO)
+    char(':')
+    minute(padding = Padding.ZERO)
+    char(':')
+    second(padding = Padding.ZERO)
 }
 
 private fun Modifier.paddingMessage(message: Message): Modifier {
